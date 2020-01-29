db17eef3ad769137d356a46d369f9cbb351df2f6
svmihar <svmihar@gmail.com>
2020-01-29 08:56:15 +0000

    :sparkles: GPU SUPPORT

--- model_textgenrnn_context_char.py
+++ model_textgenrnn_context_char.py
@@ -3,26 +3,25 @@ from datetime import datetime
 import os 
 
 model_cfg = {
-    'word_level': True,   # set to True if want to train a word-level model (requires more data and smaller max_length)
-    'rnn_size': 1200,   # number of LSTM cells of each layer (128/256 recommended)
-    'rnn_layers': 5,   # number of LSTM layers (>=2 recommended)
+    'word_level': False,   # set to True if want to train a word-level model (requires more data and smaller max_length)
+    'rnn_size': 256,   # number of LSTM cells of each layer (128/256 recommended)
+    'rnn_layers': 2,   # number of LSTM layers (>=2 recommended)
     'rnn_bidirectional': True,   # consider text both forwards and backward, can give a training boost
-    'max_length': 7,   # number of tokens to consider before predicting the next (20-40 for characters, 5-10 for words recommended)
-    'max_words': 100000,   # maximum number of words to model; the rest will be ignored (word-level model only)
+    'max_length': 30,   # number of tokens to consider before predicting the next (20-40 for characters, 5-10 for words recommended)
 }
 
 train_cfg = {
     'line_delimited': True,   # set to True if each text has its own line in the source file
-    'num_epochs': 1000,   # set higher to train the model for longer
-    'gen_epochs': 50,   # generates sample text from model after given number of epochs
+    'num_epochs': 200,   # set higher to train the model for longer
+    'gen_epochs': 5,   # generates sample text from model after given number of epochs
     'train_size': 0.5,   # proportion of input data to train on: setting < 1.0 limits model from learning perfectly
-    'dropout': 0.5,   # ignore a random proportion of source tokens each epoch, allowing model to generalize better
+    'dropout': 0.8,   # ignore a random proportion of source tokens each epoch, allowing model to generalize better
     'validation': False,   # If train__size < 1.0, test on holdout dataset; will make overall training slower
     'is_csv': True   # set to True if file is a CSV exported from Excel/BigQuery/pandas
 }
 
 file_name = '../data/title_with_le.csv'
-model_name='v1_700_3'
+model_name='v1'
 
 textgen = textgenrnn(name=model_name)
 
@@ -33,7 +32,7 @@ train_function(
     new_model=True,
     num_epochs=train_cfg['num_epochs'],
     gen_epochs=train_cfg['gen_epochs'],
-    batch_size=512,
+    batch_size=1024,
     train_size=train_cfg['train_size'],
     dropout=train_cfg['dropout'],
     validation=train_cfg['validation'],
